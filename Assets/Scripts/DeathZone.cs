01c85f4c74013a1857fb1bc75f1a7e020199f56c
alifadhul68 <202003689@student.polytechnic.bh>
2024-01-08 14:12:59 +0300

    Implement death zone under the map

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Check if the player enters the trigger
+        {
+            SceneManager.LoadScene("Manin Menu");
+        }
+    }
+}
