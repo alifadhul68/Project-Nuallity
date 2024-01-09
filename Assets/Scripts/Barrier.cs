ad486d3929f3293f418bf120ee2596bc55050fa5
alifadhul68 <202003689@student.polytechnic.bh>
2024-01-09 22:53:24 +0300

    build settings fixes

--- Assets/Scripts/Barrier.cs
+++ Assets/Scripts/Barrier.cs
@@ -1,6 +1,3 @@
-using Palmmedia.ReportGenerator.Core;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Barrier : MonoBehaviour, IInteractable
--- Assets/Scripts/Weapons/PlayerGun.cs
+++ Assets/Scripts/Weapons/PlayerGun.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Presets;
 using UnityEngine;
 using UnityEngine.UI;
 
