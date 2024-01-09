25a87b2b04b05a3e0e73a5da605ee98ba3b1cba8
SM3176 <202003176@student.polytechnic.bh>
2024-01-09 23:03:51 +0300

    deleted stuff

--- Assets/Dialogues/DialogueAsset.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu]
-public class DialogueAsset : ScriptableObject
-{
-    [TextArea] public string[] dialogue;
-}
