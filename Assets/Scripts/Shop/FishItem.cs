74c2ebdd10a248ed1c09e2cbb5a35f53b82799be
Ali Sharaf <202001386@student.polytechnic.bh>
2024-01-09 20:53:01 +0300

    added shop validation

--- Assets/Scripts/Shop/FishItem.cs
+++ Assets/Scripts/Shop/FishItem.cs
@@ -21,13 +21,20 @@ public class FishItem : MonoBehaviour, IInteractable
     {
         if (CanInteract())
         {
-            // Calculate the amount to heal, considering max health
-            // Apply healing to the player
-            PlayerGun playerGun = PlayerGun.Instance;
-            playerGun.damage += increaseDmgBy;
-            playerGun.travelTime += increaseRangeBy;
-            PopupManager.Instance.ShowPopup(title, description, 2.5f);
-            Destroy(transform.parent.gameObject);
+            if (Coin.coins >= price)
+            {
+                PlayerGun playerGun = PlayerGun.Instance;
+                playerGun.damage += increaseDmgBy;
+                playerGun.travelTime += increaseRangeBy;
+                PopupManager.Instance.ShowPopup(title, description, 2.5f);
+                Destroy(transform.parent.gameObject);
+                Coin.coins -= price;
+                Coin.UpdateCoinCountText();
+            }
+            else
+            {
+                PopupManager.Instance.ShowPopup("Not Enough Coins", "Collect More Coins", 2.5f);
+            }
         }
     }
 
